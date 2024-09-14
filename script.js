98093c0a1f92b265a732c555e9124a235bf2bb57
ulugbekbrat <168886327+ulugbekbrat@users.noreply.github.com>
2024-09-14 15:05:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+function log (){
+    let first = prompt('Son kiriting');
+    let second = prompt('ikkinchi son kiriting');
+    
+    if (first > second) {
+        console.log('Birinchisi katta');
+    
+    } else if (first < second) {
+        console.log('Ikkinchisi katta');
+    
+    
+    } else if (first == second) {
+        console.log('Ikkalasi teng');
+    }
+}
+
+log();
